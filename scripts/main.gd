ff2c933a3f5e085a3b38662a34ac783b511f1cb8
cvyndy <cindyv.2436@gmail.com>
2025-06-15 20:53:40 -0400

    main
    where the game will be mainly coded

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,3 +9,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_laptop_pressed():
+	var loaded_scene = ResourceLoader.load("res://scenes/laptop_menu.tscn")
+	var instance = loaded_scene.instantiate()
+	get_tree().root.add_child(instance)
+	pass # Replace with function body.
+
+func _on_gacha_pressed():
+	get_tree().change_scene_to_file('res://scenes/gacha_pull.tscn')
+	pass # Replace with function body.
