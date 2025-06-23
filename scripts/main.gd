d8fe049a2733febff9a4701c69f1daf5d66afda4
cvyndy <cindyv.2436@gmail.com>
2025-06-23 17:48:23 -0400

    temp assets
    temp assets i made for the game

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -19,3 +17,4 @@ func _on_laptop_pressed():
 func _on_gacha_pressed():
 	get_tree().change_scene_to_file('res://scenes/gacha_pull.tscn')
 	pass # Replace with function body.
+
